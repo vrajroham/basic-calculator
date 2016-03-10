70aff57dceb9ecc7d2437a7a9ae0bc8bd3ec4e83
vaibhavraj <vaibhavraj.developer@gmail.com>
2016-03-10 13:34:41 +0530

    First Commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"\n\tEnter A : ";
+	cin>>a;
+	cout<<"\tEnter B : ";
+	cout<<"Addition : "<<add(a,b)<<"\n";	
+	cout<<"Sub : "<<sub(a,b)<<"\n";	
+	cout<<"Mul : "<<mult(a,b)<<"\n";	
+	return 0;
+}
+
+int add(int a, int b){
+
+}
+int add(int a, int b){
+
+}
+int add(int a, int b){
+
+}
