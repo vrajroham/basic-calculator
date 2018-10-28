4810b7e2bde456e92c924801720f5b2c9afc1e7b
root <root@LAPTOP-MGUCCIS8.localdomain>
2018-10-28 21:28:41 +0530

    changes in code

--- main.cpp
+++ main.cpp
@@ -1,25 +1,37 @@
 #include<iostream>
 using namespace std;
+
+int add(int x,int y)
+{
+
+    int z;
+    z=x+y;
+    return z;
+}
+int sub(int x,int y)
+{
+
+    int z;
+    z=x+y;
+    return z;
+}
+int mult(int x,int y)
+{
+
+    int z;
+    z=x*y;
+    return z;
+}
+
 int main(){
 	int a,b;
 	cout<<"\n\tEnter A : ";
 	cin>>a;
 	cout<<"\tEnter B : ";
-	cout<<"Addition : "<<add(a,b)<<"\n";	
-	cout<<"Sub : "<<sub(a,b)<<"\n";	
-	cout<<"Mul : "<<mult(a,b)<<"\n";	
+	cin>>b;
+	cout<<"Addition : " << add(a,b)<<"\n";
+	cout<<"Sub : "<<sub(a,b)<<"\n";
+	cout<<"Mul : "<<mult(a,b)<<"\n";
 	return 0;
 }
-
-int add(int a, int b){
-
-a=a+b;
-return a;
-
-}
-int sub(int a, int b){
- return a-b;
-}
-int add(int a, int b){
-
-}
+//bugs fixed
