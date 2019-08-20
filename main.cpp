cb16a91a1e48139d8f4c8529fc347d5acf1651db
icecream2727 <52705497+icecream2727@users.noreply.github.com>
2019-08-20 23:26:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include<iostream>
 using namespace std;
-int main(){
-	int a,b;
-	cout<<"\n\tEnter A : ";
-	cin>>a;
-	cout<<"\tEnter B : ";
-	cout<<"Addition : "<<add(a,b)<<"\n";	
-	cout<<"Sub : "<<sub(a,b)<<"\n";	
-	cout<<"Mul : "<<mult(a,b)<<"\n";	
+
+int add(int a, int b) {
+	return a + b;
+}
+int sub(int a, int b) {
+	return a - b;
+}
+int mult(int a, int b) {
+	return a * b;
+}
+
+int main() {
+	int a, b;
+	cout << "\n\tEnter A : ";
+	cin >> a;
+	cout << "\n\tEnter B : ";
+	cin >> b;
+	cout << "Addition : " << add(a, b) << "\n"
+	     << "Sub : " << sub(a, b) << "\n"
+             << "Mul : " << mult(a, b) << "\n";
 	return 0;
-}
-
-int add(int a, int b){
-
-a=a+b;
-return a;
-
-}
-int sub(int a, int b){
- return a-b;
-}
-int add(int a, int b){
-
 }
